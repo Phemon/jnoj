7fb19890296e85a656f965d216cfb799fae40160
Phemon <i@phemon.me>
2019-05-30 18:36:07 +0800

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,7 +35,7 @@ AppAsset::register($this);
                 <div class="logo pull-left">
                     <div class="pull-left">
                         <a class="navbar-brand" href="<?= Yii::$app->request->baseUrl ?>">
-                            <h2>SJZEZ</h2>
+                            <img src="<?= Yii::getAlias('@web') ?>/images/logo.png" />
                         </a>
                     </div>
                     <div class="brand">
